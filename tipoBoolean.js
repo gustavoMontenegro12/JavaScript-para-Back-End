706de822019553bd2f68ad6acc8cb69eb2b2a217
Gustavo Montenegro <gustavomontenegro.tavares@gmail.com>
2022-01-25 21:31:47 -0300

    exemplos da Aula 01 - tipos primitivos - exercício próprio

--- tipoBoolean.js
+++ tipoBoolean.js
@@ -1,7 +1,15 @@
-let total = 10
+let total = 13
 
-for(let i = 0; i <= total; i++){
+//for que checa se as duas variáveis tem o mesmo valor, caso não, a variável i é incrementada até que os numeros sejam iguais
+
+for(let i = 0; i <= 20; i++){
+    
+    let result = i == total
     console.log("Número: " + i)
-    console.log("Resultado: ", i == total)
+    console.log("Resultado: ", result)
+
+    if(result == true){
+        break
+    }
 
 }
